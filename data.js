d0719e0789e671c9e7fb904b916bd0101374c941
Patrick <patrick881199@gmail.com>
2021-03-21 16:00:19 +1100

    change some static data

--- data.js
+++ data.js
@@ -1,170 +1,170 @@
 let data = {
   "7da7672c-8ec0-4cb2-8497-894644b58366": {
     id: "7da7672c-8ec0-4cb2-8497-894644b58366",
-    image: "./imgs/image1.bmp",
+    image: "./imgs/entree1.bmp",
     category: "Entrees",
     filter: "Vegan",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "a8c3e92b-0966-4307-bb26-28381c1a226a": {
     id: "a8c3e92b-0966-4307-bb26-28381c1a226a",
-    image: "./imgs/image2.bmp",
+    image: "./imgs/entree2.bmp",
     category: "Entrees",
     filter: "Vegetarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "b02375ab-961f-4759-b245-5fab2c052498": {
     id: "b02375ab-961f-4759-b245-5fab2c052498",
-    image: "./imgs/image3.bmp",
+    image: "./imgs/entree3.bmp",
     category: "Entrees",
     filter: "Pescatarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "f7fc59e3-a728-4888-bd78-afc777559833": {
     id: "f7fc59e3-a728-4888-bd78-afc777559833",
-    image: "./imgs/image4.bmp",
+    image: "./imgs/main1.bmp",
     category: "Mains",
     filter: "Vegan",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "f1e1c65b-1d09-4fc6-9e56-3c3eb2a38e54": {
     id: "f1e1c65b-1d09-4fc6-9e56-3c3eb2a38e54",
-    image: "./imgs/image5.bmp",
+    image: "./imgs/main2.bmp",
     category: "Mains",
     filter: "Vegetarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "b375f591-a55a-45e1-92ce-d9f61048baf2": {
     id: "b375f591-a55a-45e1-92ce-d9f61048baf2",
-    image: "./imgs/image6.bmp",
+    image: "./imgs/main3.bmp",
     category: "Mains",
     filter: "Pescatarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "73484e7b-f06f-4c5a-9db0-e23a5f4c2063": {
     id: "73484e7b-f06f-4c5a-9db0-e23a5f4c2063",
-    image: "./imgs/image7.bmp",
+    image: "./imgs/salad1.bmp",
     category: "Salads",
     filter: "Vegan",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "e70b8c32-7c5e-4730-8569-20a0410440f7": {
     id: "e70b8c32-7c5e-4730-8569-20a0410440f7",
-    image: "./imgs/image8.bmp",
+    image: "./imgs/salad2.bmp",
     category: "Salads",
     filter: "Vegetarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "dd456a76-66c7-4eef-b135-31adc2d7157e": {
     id: "dd456a76-66c7-4eef-b135-31adc2d7157e",
-    image: "./imgs/image9.bmp",
+    image: "./imgs/salad3.bmp",
     category: "Salads",
     filter: "Pescatarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "e2a896b6-8af5-40c8-9f91-144191c6b7f6": {
     id: "e2a896b6-8af5-40c8-9f91-144191c6b7f6",
-    image: "./imgs/image10.bmp",
+    image: "./imgs/sides1.bmp",
     category: "Sides",
     filter: "Vegan",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
 
   "8f7885fc-3755-45b3-b134-1b640124bc38": {
     id: "8f7885fc-3755-45b3-b134-1b640124bc38",
-    image: "./imgs/image11.bmp",
+    image: "./imgs/sides2.bmp",
     category: "Sides",
     filter: "Vegetarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
 
   "403d08b3-89e9-4aa2-b5bd-455950ca758f": {
     id: "403d08b3-89e9-4aa2-b5bd-455950ca758f",
-    image: "./imgs/image12.bmp",
+    image: "./imgs/sides3.bmp",
     category: "Sides",
     filter: "Pescatarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "3f069292-bac0-40b6-9af3-4acf6b7f5859": {
     id: "3f069292-bac0-40b6-9af3-4acf6b7f5859",
-    image: "./imgs/image13.bmp",
+    image: "./imgs/dessert1.bmp",
     category: "Desserts",
     filter: "Vegan",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
 
   "77a1bfcd-fd96-4c95-9b0a-e4840227f55e": {
     id: "77a1bfcd-fd96-4c95-9b0a-e4840227f55e",
-    image: "./imgs/image14.bmp",
+    image: "./imgs/dessert2.bmp",
     category: "Desserts",
     filter: "Vegetarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "bce66551-574e-42d1-a899-1f8afbe996ab": {
     id: "bce66551-574e-42d1-a899-1f8afbe996ab",
-    image: "./imgs/image15.bmp",
+    image: "./imgs/dessert3.bmp",
     category: "Desserts",
     filter: "Pescatarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
 
   "001ac849-ac45-4f6b-9069-50e43e75cfa6": {
     id: "001ac849-ac45-4f6b-9069-50e43e75cfa6",
-    image: "./imgs/image16.bmp",
+    image: "./imgs/drinks1.bmp",
     category: "Drinks",
     filter: "Vegan",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
   "95568d22-ef0b-4adc-8844-2e75f65f1d97": {
     id: "95568d22-ef0b-4adc-8844-2e75f65f1d97",
-    image: "./imgs/image17.bmp",
+    image: "./imgs/drinks2.bmp",
     category: "Drinks",
     filter: "Vegetarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
 
   "12bc2c40-8996-4fb3-8d7f-485a7efe1d4f": {
     id: "12bc2c40-8996-4fb3-8d7f-485a7efe1d4f",
-    image: "./imgs/image18.bmp",
+    image: "./imgs/drinks33.bmp",
     category: "Drinks",
     filter: "Pescatarian",
-    name: "Lorem ipsum dolor sit amet.",
+    name: "Lorem ipsum dt amet.",
     description:
-      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
+      "temporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio commtemporibus placeat, molestias voluptas quae voluptates amet quiobcaecati veniam fuga mollitia distinctio comm",
   },
 };
 function uuidv4() {
